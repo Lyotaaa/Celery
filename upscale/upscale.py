c193329fe132a8c482f237fdbffd327eaac07ff9
Lyota <korystin.aleksei@yandex.ru>
2023-10-08 11:27:18 +0300

    update file upscale

--- upscale/upscale.py
+++ upscale/upscale.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 from celery import Celery
 from cv2 import dnn_superres
@@ -6,11 +7,11 @@ from cv2 import dnn_superres
 backend = "redis://127.0.0.1:6379/1"
 broker = "redis://127.0.0.1:6379/2"
 
-celery = Celery("app", backend=backend, broker=broker)
+celery = Celery("main", backend=backend, broker=broker)
 
 
 @celery.task
-def upscale(input_path: str, output_path: str, model_path: str = "EDSR_x2.pb"):
+def upscale(input_path: str, output_path: str = "lama_600px.png", model_path: str = "EDSR_x2.pb"):
     """
     :param input_path: путь к изображению для апскейла
     :param output_path:  путь к выходному файлу
@@ -24,4 +25,13 @@ def upscale(input_path: str, output_path: str, model_path: str = "EDSR_x2.pb"):
     image = cv2.imread(input_path)
     result = scaler.upsample(image)
     cv2.imwrite(output_path, result)
-    return output_path
+
+def example():
+    # upscale('upscale/lama_300px.png', os.path.join('results', 'lama_600px.png'))
+    input_path = os.path.join("lama_300px.png")
+    output_path = os.path.join("../results", "lama_600px.png")
+    model_path = os.path.join("EDSR_x2.pb")
+    upscale(input_path, output_path, model_path)
+
+if __name__ == "__main__":
+    example()
